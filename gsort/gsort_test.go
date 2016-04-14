99bc7a3c211ff0524b4b30a0d1b4f0953d4a2dfb
Brent Pedersen <bpederse@gmail.com>
2016-04-13 18:46:18 -0600

    failing test

--- gsort/gsort_test.go
+++ gsort/gsort_test.go
@@ -1,8 +1,12 @@
 package gsort_test
 
 import (
+	"bytes"
+	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/brentp/ggd-utils/gsort"
 
 	. "gopkg.in/check.v1"
 )
@@ -17,15 +21,24 @@ func (s *GSortTest) TestSort(c *C) {
 
 	data := strings.NewReader(`a\t1\nb\t2\na\t3`)
 
-	pp := func(line []byte) LineDeco {
-        l := LineDeco{Cols:make([]int, 2}
-		toks := bytes.Split(line, []byte('\t'))
-		if toks[0][0] == 'a' {
-			l[0] = 0
+	pp := func(line []byte) gsort.LineDeco {
+		l := gsort.LineDeco{Cols: make([]int, 2)}
+		toks := bytes.Split(line, []byte{'\t'})
+		l.Cols[0] = int(toks[0][0])
+		v, err := strconv.Atoi(string(toks[1]))
+		if err != nil {
+			l.Cols[1] = -1
+		} else {
+			l.Cols[1] = v
 		}
-		
-	}
+		return l
 
-	Sortkk
+	}
+	var wtr *bytes.Buffer
+
+	err := gsort.Sort(data, wtr, pp, 22)
+	c.Assert(err, IsNil)
+
+	c.Assert(wtr.String(), Equals, "xx")
 
 }
